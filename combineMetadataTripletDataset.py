87daedca817047bc0866b0e299dc95154e6005e4
Swarali Surana <swaralis123@gmail.com>
2022-05-02 05:14:51 -0400

    Add files via upload

--- combineMetadataTripletDataset.py
+++ combineMetadataTripletDataset.py
@@ -1,14 +1,16 @@
 #script to merge the triplet and metadata file
 import pandas
 
+triplets_file = r"train_triplets\train_triplets.txt"
+songs_metadata_file = r"SongCSV.csv"
 
-dataPath = '../dataset/Million Song/'
-triplets_file = dataPath + "triplets.csv"
-songs_metadata_file = dataPath + "unique_tracks.csv"
+song_df_1 = pandas.read_table(triplets_file,header=None)
+song_df_1.columns = ['user_id', 'song_id', 'listen_count']
+#print(song_df_1[1:10])
 
-song_df_1 = pandas.read_csv(triplets_file)
 song_df_2 =  pandas.read_csv(songs_metadata_file)
+song_df_2.rename(columns = {'SongID':'song_id'}, inplace = True)
 
-song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['SongID']), on="SongID", how="inner")
+song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
 
-song_df.to_csv(dataPath + 'songCSV.csv', index = False)
+song_df.to_csv('out.csv')
