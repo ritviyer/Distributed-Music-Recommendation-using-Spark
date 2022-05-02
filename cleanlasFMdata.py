33a7dda74b60ae22e03ad17bb577948e3f985ab6
Ritvik Iyer <77076795+ritviyer@users.noreply.github.com>
2022-05-02 02:33:47 -0400

    Update cleanlasFMdata.py

--- cleanlasFMdata.py
+++ cleanlasFMdata.py
@@ -2,35 +2,35 @@ import pandas as pd
 
 dataPath = '../dataset/hetrec2011-lastfm-2k/'
 
-# df = pd.read_csv(dataPath + 'user_artists.dat')
-# df = df['userID\tartistID\tweight']
-# newList = []
-# for ind, row in df.iteritems():
-#     newList.append(row.split('\t'))
+df = pd.read_csv(dataPath + 'user_artists.dat')
+df = df['userID\tartistID\tweight']
+newList = []
+for ind, row in df.iteritems():
+    newList.append(row.split('\t'))
 
-# newList = pd.DataFrame(newList,columns=['userID','artistID','Weight'])
-# newList = newList.apply(pd.to_numeric)
-# newList.to_csv(dataPath+'user_artists.csv', index=False)
-
-
-
-# df = pd.read_csv(dataPath + 'tags.dat')
-# df = df['tagID\ttagValue']
-# newList = []
-# for ind, row in df.iteritems():
-#     newList.append(row.split('\t'))
-
-# newList = pd.DataFrame(newList,columns=['tagID','tagValue'])
-# newList.to_csv(dataPath+'tags.csv', index=False)
+newList = pd.DataFrame(newList,columns=['userID','artistID','Weight'])
+newList = newList.apply(pd.to_numeric)
+newList.to_csv(dataPath+'user_artists.csv', index=False)
 
 
 
-# df = pd.read_csv(dataPath + 'user_friends.dat')
-# df = df['userID\tfriendID']
-# newList = []
-# for ind, row in df.iteritems():
-#     newList.append(row.split('\t'))
+df = pd.read_csv(dataPath + 'tags.dat')
+df = df['tagID\ttagValue']
+newList = []
+for ind, row in df.iteritems():
+    newList.append(row.split('\t'))
 
-# newList = pd.DataFrame(newList,columns=['userID','friendID'])
-# newList = newList.apply(pd.to_numeric)
-# newList.to_csv(dataPath+'user_friends.csv', index=False)
+newList = pd.DataFrame(newList,columns=['tagID','tagValue'])
+newList.to_csv(dataPath+'tags.csv', index=False)
+
+
+
+df = pd.read_csv(dataPath + 'user_friends.dat')
+df = df['userID\tfriendID']
+newList = []
+for ind, row in df.iteritems():
+    newList.append(row.split('\t'))
+
+newList = pd.DataFrame(newList,columns=['userID','friendID'])
+newList = newList.apply(pd.to_numeric)
+newList.to_csv(dataPath+'user_friends.csv', index=False)
